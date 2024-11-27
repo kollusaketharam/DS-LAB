d1364f41ed2fe2a4581ace2a69b13cc469d0fff3
kollusaketharam <kollusaketha.cs23@bmsce.ac.in>
2024-11-27 12:41:18 +0530

    Add files via upload

---
+++ Untitled1.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Node* createNode(int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+struct Node* insert(struct Node* root, int value) {
+    if (root == NULL) {
+        return createNode(value);
+    }
+    if (value < root->data) {
+        root->left = insert(root->left, value);
+    } else if (value > root->data) {
+        root->right = insert(root->right, value);
+    }
+    return root;
+}
+
+void inOrder(struct Node* root) {
+    if (root != NULL) {
+        inOrder(root->left);
+        printf("%d ", root->data);
+        inOrder(root->right);
+    }
+}
+
+void preOrder(struct Node* root) {
+    if (root != NULL) {
+        printf("%d ", root->data);
+        preOrder(root->left);
+        preOrder(root->right);
+    }
+}
+
+void postOrder(struct Node* root) {
+    if (root != NULL) {
+        postOrder(root->left);
+        postOrder(root->right);
+        printf("%d ", root->data);
+    }
+}
+
+void displayTree(struct Node* root) {
+    if (root == NULL) {
+        printf("The tree is empty.\n");
+        return;
+    }
+
+    printf("In-order Traversal: ");
+    inOrder(root);
+    printf("\n");
+
+    printf("Pre-order Traversal: ");
+    preOrder(root);
+    printf("\n");
+
+    printf("Post-order Traversal: ");
+    postOrder(root);
+    printf("\n");
+}
+
+int main() {
+    struct Node* root = NULL;
+    int value, n;
+
+    printf("Enter the number of values to insert into the tree: ");
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        printf("Enter value %d: ", i + 1);
+        scanf("%d", &value);
+        root = insert(root, value);
+    }
+
+    displayTree(root);
+
+    return 0;
+}
+
